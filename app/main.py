0e67c5f7128a77f8d6f2bc631ab833bfa987e1f7
Pavel Drovnin <pavel98rus@gmail.com>
2025-03-20 16:14:05 +0300

    lifespan, static

--- app/main.py
+++ app/main.py
@@ -1,4 +1,19 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+from api.v1 import router as v1_router
+from core.config import STATIC
+from db.database import create_tables_and_admin_user
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_tables_and_admin_user()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.mount('/static', StaticFiles(directory=STATIC), name='static')
+app.include_router(v1_router)
